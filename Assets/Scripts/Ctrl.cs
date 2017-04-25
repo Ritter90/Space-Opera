31cc0ecfd53e07c7eba2c117c67b020b4ac42ffb
Ritter90 <Ritter90@users.noreply.github.com>
2017-04-25 14:00:39 +0100

    Copy over Ctrl script

---
+++ Assets/Scripts/Ctrl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ctrl : MonoBehaviour {
+
+	public float speed = 15.5f;
+
+	void FixedUpdate()
+	{
+		Debug.Log ("FixedUpdate" + Time.time);
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position += Vector3.up * speed * Time.deltaTime;
+
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position += Vector3.down * speed * Time.deltaTime;
+		}
+	}
+
+	void Update(){
+		Debug.Log ("Update" + Time.time);
+	}
+}
