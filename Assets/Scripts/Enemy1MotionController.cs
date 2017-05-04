d38096f2b04eecb01413af1ec21835256f5f4349
don6l <donalmcclean@gmail.com>
2017-05-04 17:28:07 +0100

    all added together

---
+++ Assets/Scripts/Enemy1MotionController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy1MotionController : MonoBehaviour {
+
+	public bool inPlay = false;
+	public float speed;
+	public float verticalRange;
+
+	public float inPlayX;
+	public float outOfPlayX;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 currentPos = transform.position;
+		if (inPlay)
+		{
+			currentPos.y = Mathf.Sin(Time.time * speed) * verticalRange;            
+			if(currentPos.x != inPlayX)
+			{
+				currentPos.x = Mathf.Lerp(currentPos.x, inPlayX, Time.deltaTime * speed);
+			}
+
+		}
+		else
+		{
+			if (currentPos.x != outOfPlayX)
+			{
+				currentPos.x = Mathf.Lerp(currentPos.x, outOfPlayX, Time.deltaTime * speed);
+			}
+		}
+		transform.position = currentPos;
+	}
+}
