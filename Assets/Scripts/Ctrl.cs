957a33d5f0f58c53f7b2e708f566cebdcc02b509
don6l <donalmcclean@gmail.com>
2017-05-04 13:26:29 +0100

    update

--- Assets/Scripts/Ctrl.cs
+++ Assets/Scripts/Ctrl.cs
@@ -7,10 +7,11 @@ public class Ctrl : MonoBehaviour {
 	public Quaternion originalRotationValue;
 	public Quaternion maxUpRotationValue;
 	public Quaternion minDownRotationValue;
-	float rotationResetSpeed = 0.1f;
+	public float rotationResetSpeed = 1f;
+	public float rotationSpeed = 1f;
     public float upperBound = 5f;
     public float lowerBound = -5f;
-
+	public float rotationBound = 0.2f;
 
 	void start(){
 		originalRotationValue = transform.rotation; // save the initial rotation
@@ -21,54 +22,41 @@ public class Ctrl : MonoBehaviour {
     {
         float input = Input.GetAxisRaw("Vertical");
         MoveShipPosition(input);
+		RotateShip (input);
+
     }
 
-	void Update (){
-		if (Input.GetKey ("down")) {
-			if (transform.rotation.z > -0.4) {
-				gameObject.transform.Rotate(0, 0, -1);
-			}
+	private void MoveShipPosition(float input)
+	{
 
+		float newYPos = transform.position.y + (speed * Time.deltaTime * input);
+		if (newYPos > upperBound)
+		{
+			transform.position = new Vector3(transform.position.x, upperBound);
+		}
+		else if (newYPos < lowerBound)
+		{
+			transform.position = new Vector3(transform.position.x, lowerBound);
+		}
+		else
+		{
+			transform.position = new Vector3(transform.position.x, newYPos);
 		}
 
-		else if (Input.GetKey ("up")) {
-			
-			if (transform.rotation.z < 0.4) {
-				gameObject.transform.Rotate (0, 0, 1);
-			}
 
-		} 
-		else {
-				gameObject.transform.rotation = Quaternion.Slerp (transform.rotation, originalRotationValue, Time.time * rotationResetSpeed);
-
-		}
-				
 	}
 
-    private void MoveShipPosition(float input)
-    {
 
+	private void RotateShip(float input){
+		if (input != 0) {
+			if (transform.rotation.z > -rotationBound && transform.rotation.z < rotationBound) {	
+				gameObject.transform.Rotate (0, 0, 1 * rotationSpeed * input);
+			}
 
-        float newYPos = transform.position.y + (speed * Time.deltaTime * input);
-        if (newYPos > upperBound)
-        {
-		//	transform.Rotate(0, 0, 10);
-		//	print ("Up" + transform.rotation);
-            transform.position = new Vector3(transform.position.x, upperBound);
-        }
-        else if (newYPos < lowerBound)
-        {
-		//	transform.Rotate(0, 0, -10);
-		//	print ("Down" + transform.rotation);
-		//	transform.rotation = new Vector3 (0,0,-10);
-            transform.position = new Vector3(transform.position.x, lowerBound);
-        }
-        else
-        {
-		//	transform.Rotate(0, 0, 0);
-		//	print ("Stay" + transform.rotation);
-		//	transform.rotation = new Vector3 (0,0,0);
-            transform.position = new Vector3(transform.position.x, newYPos);
-        }
-    }
+		} else {
+			gameObject.transform.rotation = originalRotationValue; 
+		}
+
+	}
+		
 }
